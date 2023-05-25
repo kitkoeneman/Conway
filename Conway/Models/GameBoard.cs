9475b49502835984b49ac50318af87716dfa2747
Kit Koeneman <59888890+kitkoeneman@users.noreply.github.com>
2023-05-25 18:30:14 +0100

    Home page renders with two text boxes for height and width.

---
+++ Conway/Models/GameBoard.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Conway.Models;
+
+
+public class GameBoard
+{
+    [BindProperty]
+    [Range(10,100)]
+    public int Height { get; set; }
+    
+    [BindProperty]
+    [Range(10,100)]
+    public int Width { get; set; }
+}
